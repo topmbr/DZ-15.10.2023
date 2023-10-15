42f515cefd4d8e976f9b11f517194d246213762d
topmbr <centerm9911@gmail.com>
2023-10-15 18:23:58 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp29/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace WindowsFormsApp29
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.textBoxFunction = new System.Windows.Forms.TextBox();
+            this.buttonPlot = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(0, 0);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(519, 437);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // textBoxFunction
+            // 
+            this.textBoxFunction.Location = new System.Drawing.Point(555, 44);
+            this.textBoxFunction.Name = "textBoxFunction";
+            this.textBoxFunction.Size = new System.Drawing.Size(159, 22);
+            this.textBoxFunction.TabIndex = 1;
+            // 
+            // buttonPlot
+            // 
+            this.buttonPlot.Location = new System.Drawing.Point(555, 88);
+            this.buttonPlot.Name = "buttonPlot";
+            this.buttonPlot.Size = new System.Drawing.Size(159, 23);
+            this.buttonPlot.TabIndex = 2;
+            this.buttonPlot.Text = "button1";
+            this.buttonPlot.UseVisualStyleBackColor = true;
+            this.buttonPlot.Click += new System.EventHandler(this.buttonPlot_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonPlot);
+            this.Controls.Add(this.textBoxFunction);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.TextBox textBoxFunction;
+        private System.Windows.Forms.Button buttonPlot;
+    }
+}
+
---
+++ WindowsFormsApp29/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using ZedGraph;
+using NCalc;
+
+namespace WindowsFormsApp29
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Инициализация элемента управления ZedGraphControl
+            InitializeGraph();
+
+        }
+        private void InitializeGraph()
+        {
+            GraphPane graphPane = zedGraphControl1.GraphPane;
+            graphPane.CurveList.Clear();
+
+            // Настройки для осей и подписей
+            graphPane.XAxis.Title.Text = "X";
+            graphPane.YAxis.Title.Text = "Y";
+
+            // Обновление отображения графика
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+        }
+
+        private void PlotGraph(string function)
+        {
+            GraphPane graphPane = zedGraphControl1.GraphPane;
+            graphPane.CurveList.Clear();
+
+            // Создание списка точек для графика
+            PointPairList points = new PointPairList();
+            Expression expression = new Expression(function);
+            for (float x = -10; x <= 10; x += 0.1f)
+            {
+                expression.Parameters["x"] = x;
+                float y = (float)expression.Evaluate(); // Используем float для хранения значений
+                points.Add(x, y);
+            }
+
+            // Создание кривой для графика
+            LineItem curve = graphPane.AddCurve("График функции", points, Color.Blue, SymbolType.None);
+
+            // Обновление отображения графика
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+        }
+
+        private void buttonPlot_Click_1(object sender, EventArgs e)
+        {
+            string function = textBoxFunction.Text;
+            PlotGraph(function);
+        }
+    }
+
+}
